33f2ed2550f22d98a5c02b2e4b0ad83451d565cf
xiafan68 <xiafan68@gmail.com>
2015-10-23 17:01:07 +0800

    interactive

--- src/main/scala/example/SparkInteractiveExapmle.scala
+++ src/main/scala/example/SparkInteractiveExapmle.scala
@@ -1,111 +1,78 @@
 package example
 
-import java.io.StringReader
-
-import scala.collection.mutable.ArrayBuffer
-import scala.reflect.runtime.universe
-
 import org.apache.spark.SparkConf
-import org.apache.spark.rdd.RDD.rddToOrderedRDDFunctions
-import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
-import org.apache.spark.sql.hive.HiveContext
-import org.apache.spark.sql.hive.thriftserver.HiveThriftServer2
-import org.apache.spark.streaming.Seconds
-import org.apache.spark.streaming.StreamingContext
-import org.apache.spark.streaming.dstream.DStream.toPairDStreamFunctions
-import org.wltea.analyzer.core.IKSegmenter
-import org.wltea.analyzer.core.Lexeme
-
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 
 /**
- * 统计每个时间点的热门搜索词，这里使用的是sogou的搜索日志
- *
  * @author xiafan
  */
-object StreamingContext {
-  case class WordCounts(word: String, count: Int)
+object SparkInteractiveExapmle {
+  def main(args: Array[String]): Unit = {
 
-  def main(args: Array[String]) {
-    val opt: Map[Symbol, Any] = Map[Symbol, Any]()
-    def usage(): Unit = System.err.println("Usage: HdfsWordCount --input inputdir --stopwords stopfile")
-    def nextOption(map: Map[Symbol, Any], list: List[String]): Map[Symbol, Any] = {
-      def isSwitch(s: String) = (s(0) == '-')
-      list match {
-        case Nil => map
-        case "--input" :: value :: tail =>
-          nextOption(map ++ Map('input -> value), tail)
-        case "--stopwords" :: value :: tail =>
-          nextOption(map ++ Map('stopwords -> value), tail)
-        //case string :: opt2 :: tail if isSwitch(opt2) =>
-        //  nextOption(map ++ Map('infile -> string), list.tail)
-        case option :: tail =>
-          println("Unknown option " + option)
-          usage()
-          System.exit(1)
-          map
-      }
+  }
+
+  class ClosureClass(val message: String) extends Serializable {
+    def transform(line: String): String = {
+      return line + this.message
     }
-    val options = nextOption(Map(), args.toList)
-    if (options.size != 2) {
-      usage()
-      System.exit(1)
-    }
-    println("options are " + options.toString())
-    // Create the context
-    val conf = new SparkConf()
-    conf.setAppName("hdfswordcount")
-    val ssc = new StreamingContext(conf, Seconds(10))
+  }
 
-    // 创建一个FileInputDStream监控给定的目录
-    val lines = ssc.textFileStream(options('input).toString())
-    //统计新文件中单词的次数，这里首先对每个查询中的查询词进行分词
-    val words = lines.mapPartitions(
-      lines => {
-        val segmenter = new IKSegmenter(new StringReader(""), true)
-        lines.map(line => {
-          val words = new ArrayBuffer[String]()
-          segmenter.reset(new StringReader(line))
-          var token: Lexeme = null
-          var hasNext = true
-          while (hasNext) {
-            token = segmenter.next()
-            if (token != null) {
-              // println()
-              words += token.getLexemeText()
-            } else {
-              hasNext = false
-            }
-          }
-          words.toArray
-        })
-      }).flatMap { x => x.map(word => word) }
+  def planRDDOp(): Unit = {
+    val sc = new SparkContext(new SparkConf())
+    val logs = sc.textFile("file:/Users/xiafan/Documents/dataset/sparklecture/interactive/appfirewall.log")
 
-    //加载通用词，有兴趣的同学可以考虑使用broadcast变量来实现一下
-    val sc = ssc.sparkContext
-    val stopWords = sc.textFile(options('stopwords).toString()).map(x => (x, null))
-    //这里将dstream和rdd进行操作
-    val filteredRdd = words.transform(rdd => rdd.map(x => (x, null)).join(stopWords).map(x => x._1))
+    logs.count()
 
-    //周期性计算过去60秒内单词搜索次数
-    val peroidCounts = filteredRdd.map(x => (x, 1)).reduceByKeyAndWindow(_ + _, _ - _, Seconds(20), Seconds(10))
-    val sqlContext = new HiveContext(ssc.sparkContext)
-    import sqlContext.implicits._
-    peroidCounts.foreachRDD { rdd =>
-      // Convert RDD to DataFrame and register it as a SQL table
-      val wordCountsDataFrame = rdd.map(x => WordCounts(x._1, x._2)).toDF()
-      wordCountsDataFrame.registerTempTable("word_counts")
-    }
-    // Start the JDBC server
-    HiveThriftServer2.startWithContext(sqlContext)
+    logs.first()
+    logs.take(10).foreach(println)
 
-    //统计每个rdd里面的最热词
-    val wordCounts = filteredRdd.map(x => (x, 1)).reduceByKey(_ + _)
-    val sortedWords = wordCounts.map(x => (x._2, x._1)).transform(rdd => rdd.sortByKey())
-    sortedWords.print(10)
-    peroidCounts.saveAsTextFiles("peroid", "pop")
+    //过滤
+    val eclipseLog = logs.filter(line => line.contains("eclipse"))
+    eclipseLog.take(10)
+    val javaLog = logs.filter(line => line.contains("java"))
+    javaLog.take(10)
+    //一到多
+    eclipseLog.flatMap(line => line.split(":")).take(10).foreach(println)
+    //一到一
+    eclipseLog.map(line => line.split(":")(3)).take(10).foreach(println)
+    //对每个partition执行map函数
+    eclipseLog.mapPartitionsWithIndex((idx, lines) => lines.map(x => x.split(":")(2))).take(10).foreach(println)
 
-    ssc.checkpoint("hdfs://10.11.1.42/user/xiafan/check")
-    ssc.start()
-    ssc.awaitTermination()
+    //闭包验证
+    val func = new ClosureClass("this is toy example")
+    eclipseLog.map(line => func.transform(line)).take(10).foreach(println)
+
+    //集合操作
+    val unionLog = eclipseLog.union(javaLog)
+    unionLog.count()
+
+    val optPrograms = logs.map(line => {
+      val fields = line.split(":")
+      if (fields.length >= 4)
+        Some(fields(3).trim())
+      else
+        None
+    })
+
+    val programs = optPrograms.filter(rec => !rec.isEmpty).map(f => f.get)
+
+    programs.distinct().take(10)
+    val selProgs = sc.parallelize(List("eclipse", "chromedriver", "kuaipan"))
+    programs.intersection(selProgs).take(10)
+
+    val progCount = programs.map(x => (x, 1))
+    progCount.groupByKey().take(1)
+    progCount.reduceByKey(_ + _).take(10)
+    progCount.cogroup(selProgs.map(x => (x, 0))).take(1)
+    progCount.join(selProgs.map(x => (x, 0))).take(1)
+
+    val progOcurrs = progCount.reduceByKey(_ + _)
+    progOcurrs.map(rec => (rec._2, rec._1)).sortByKey(false).take(1)
+    progCount.aggregateByKey(0)((value, rec) => value + 1, _ + _)
+
+    progCount.combineByKey(value => value,
+      (state: Int, value) => state + value,
+      (state1: Int, state2: Int) => state1 + state2).take(5)
   }
 }
